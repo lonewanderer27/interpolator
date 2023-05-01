84b427a2884e9a08b6bd333b68e38d32280f3309
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-05-01 17:09:58 +0800

    added the sidebar and maindiv

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import './components.css'
+
+function MainDiv() {
+    return (
+        <div className="sb">
+            <div className="sb-inner">
+                
+            </div>
+        </div>
+    )
+}
+
+export default MainDiv
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,18 @@
+import './components.css'
+
+function SideBar() {
+    return (
+        <div className="sb">
+            <div className="sb-inner">
+                <div className="sb-i-up">
+                    <h1 className='sb-i-up-logo'></h1>
+                </div>
+                <div className="sb-i-down">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideBar
