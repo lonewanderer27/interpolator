4341e0c7807a83168909bccfa8e952a5fe5ea88c
lonewanderer27 <adrianejames27@gmail.com>
2023-05-03 10:36:13 +0800

    made the layout responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import SideBar from './components/SideBar'
-import MainDiv from './components/Main'
 import './App.css'
+
+import MainDiv from './components/Main'
+import SideBar from './components/SideBar'
 
 function App() {
   return (
-    <div className="App">
-      <SideBar />
-      <MainDiv />
+    <div className="App row">
+      <div className="col-12 col-lg-4">
+        <SideBar/>
+      </div>
+      <div className="col-12 col-lg-8">
+        <MainDiv/>
+      </div>
     </div>
   )
 }
